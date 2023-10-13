496f77d7eae37314d5bc82d060a61eba478e5634
Pandiammal-2103 <144665585+Pandiammal-2103@users.noreply.github.com>
2023-10-13 15:27:03 +0530

    Add files via upload

---
+++ unit1.2.py
@@ -0,0 +1,7 @@
+n=int(input("enter a number"))       
+  f=1                             
+  i=1                          
+ While(i<=n):    
+   f=f*i                        
+    i=i+1                            
+   Print(" the factorial is",f)
